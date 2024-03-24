4d97b58e5febe37fbbc399b1d48bfa72eb16c046
Zaryab Haider <zaryabhaider88@gmail.com>
2024-03-25 00:01:09 +0500

    Commit message describing your changes

--- server/routes/Jobs.js
+++ server/routes/Jobs.js
@@ -1,62 +1,62 @@
 import express from "express";
 import bodyParser from "body-parser";
-const Jobs = require('../models/jobs');
+const Jobs = require("../models/jobs");
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.get('/jobs', async (req, res) => {
-    try {
-        const jobs = await Jobs.find();
-        res.json(jobs);
-    } catch (err) {
-        res.status(500).json({
-            message: err.message
-        });
-    }
+app.get("/", async (req, res) => {
+  try {
+    const jobs = await Jobs.find();
+    res.json(jobs);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
 });
 
-app.post('/jobs', async (req, res) => {
-    const job = new Jobs(req.body);
-    try {
-        const newJob = await job.save();
-        res.status(201).json(newJob);
-    } catch (err) {
-        res.status(400).json({
-            message: err.message
-        });
-    }
+app.post("/", async (req, res) => {
+  const job = new Jobs(req.body);
+  try {
+    const newJob = await job.save();
+    res.status(201).json(newJob);
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
 });
 
-
-app.put('/jobs/:id', async (req, res) => {
-    try {
-        const updatedJob = await Jobs.findByIdAndUpdate(req.params.id, req.body, {
-            new: true
-        });
-        res.json(updatedJob);
-    } catch (err) {
-        res.status(400).json({
-            message: err.message
-        });
-    }
+app.put("/:id", async (req, res) => {
+  try {
+    const updatedJob = await Jobs.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(updatedJob);
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
 });
 
-
-app.delete('/jobs/:id', async (req, res) => {
-    try {
-        await Jobs.findByIdAndDelete(req.params.id);
-        res.json({
-            message: 'Job listing deleted'
-        });
-    } catch (err) {
-        res.status(500).json({
-            message: err.message
-        });
-    }
+app.delete("/:id", async (req, res) => {
+  try {
+    await Jobs.findByIdAndDelete(req.params.id);
+    res.json({
+      message: "Job listing deleted",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
 });
 
 module.exports = router;
